af4eb307377434825e6b2dd05da634b950f23a0d
Nadali2002 <nadalidewindi@gmail.com>
2025-08-09 15:33:49 +0530

    clear all previous files

--- routes/players.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const playerController = require('../controllers/playerController');
-
-router.get('/', playerController.getPlayers);
-router.post('/', playerController.addPlayer);
-
-module.exports = router;
