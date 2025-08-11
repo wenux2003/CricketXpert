24b67b5e3e5a2bdf2507c9bab9a773801685ec9b
Nadali2002 <nadalidewindi@gmail.com>
2025-08-11 15:39:04 +0530

    change some tbles

--- models/CoachAvailability.js
+++ models/CoachAvailability.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const coachAvailabilitySchema = new mongoose.Schema({
-  coachId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },//Meka poddak balanna
+  coachId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   availableSlots: [{
     date: { type: Date, required: true },
     timeStart: { type: String, required: true }, 
