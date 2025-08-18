5c6b5a67bd45d8e1473d8f1c647a878342263a1b
Nadali2002 <nadalidewindi@gmail.com>
2025-08-18 11:27:22 +0530

    routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Create user
+router.post('/', userController.createUser);
+
+// Get all users
+router.get('/', userController.getAllUsers);
+
+// Get user by ID
+router.get('/:id', userController.getUserById);
+
+// Update user
+router.put('/:id', userController.updateUser);
+
+// Delete user
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
