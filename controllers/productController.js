af8da792dc568d51a7c64a5d68cf2d567bdbf9fa
HimashaIndiwari <indiwari2022@gmail.com>
2025-08-23 22:37:12 +0530

    order

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Product = require('../models/product');
+const Product = require('./models/product');
 
 // Create product
 exports.createProduct = async (req, res) => {
