b7d06bd874ade9e7f9893a21a2422a73640a646e
PabodaWA <wampaboda@gmail.com>
2025-08-25 20:28:34 +0530

    change to localhost 5000

--- coaching-dashboard/vite.config.js
+++ coaching-dashboard/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000,
+    strictPort: true,
+    open: true,
+  },
 })
+
+
