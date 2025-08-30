65cdbb96fc99361be933ab6dc2725fe6d230c5b6
Nadali2002 <nadalidewindi@gmail.com>
2025-08-30 16:12:30 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
-  passwordHash: { type: String, required: true },
   role: {
     type: String,
     enum: ['admin', 'coach', 'technician', 'customer','coaching_manager' ,'order_manager', 'ground_manager', 'service_manager', 'delivery_staff'],
@@ -18,7 +17,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     enum: ['active', 'suspended', 'deactivated'],
     default: 'active',
-  }
+  },
+  dob: Date,
+  // --- ADD THESE TWO FIELDS FOR PASSWORD RESET ---
+  passwordResetCode: String,
+  passwordResetExpires: Date,
+
 }, { timestamps: true }); // <-- This automatically adds and manages createdAt & updatedAt
 
 const User = mongoose.model('User', userSchema);
